d09064a42c508080b469431f15093387ec6c6f0d
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 21:42:45 -0300

    Injected ProjectService

--- src/app/projects/project-list/project-list.component.ts
+++ src/app/projects/project-list/project-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../project.service';
+import { Project } from 'src/app/model/project';
 
 @Component({
   selector: 'app-project-list',
   templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.css']
+  styleUrls: ['./project-list.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectListComponent implements OnInit {
 
-  constructor() { }
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit(): void {
+    this.projects = this.projectService.getProjects()
   }
 
 }
