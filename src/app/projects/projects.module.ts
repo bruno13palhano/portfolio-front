1a23d3aae28be97eb935d9891f65c6a1bca33d2a
bruno13palhano <bruno13palhano@gmail.com>
2023-05-28 21:43:46 -0300

    Added componentes to declarations

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { FavoriteProjectsComponent } from './favorite-projects/favorite-projects.component';
+import { ProjectComponent } from './project/project.component';
 
 
 
 @NgModule({
   declarations: [
-    ProjectListComponent
+    ProjectListComponent,
+    FavoriteProjectsComponent,
+    ProjectComponent
   ],
   imports: [
     CommonModule
