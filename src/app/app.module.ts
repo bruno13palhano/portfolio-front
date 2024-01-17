cd2a01661989350c227dd30fb3fbd1dffd5a1541
bruno13palhano <bruno13palhano@gmail.com>
2024-01-17 19:41:21 -0300

    Remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectComponent } from './projects/project/project.component';
 import { SortPipe } from './sort.pipe';
 import { CopyrightDirective } from './copyright.directive';
 import { ProjectsModule } from './projects/projects.module';
