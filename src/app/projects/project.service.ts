da009f21bc2280272532f38f95f7ec7929045ead
bruno13palhano <bruno13palhano@gmail.com>
2024-01-20 19:48:12 -0300

    Refactor

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -3,7 +3,7 @@ import { Project } from './project';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const projectUrl = 'http://localhost:8080/projects/';
+const projectUrl = 'http://localhost:8080/projects';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +13,10 @@ export class ProjectService {
   constructor(private http: HttpClient) { }
 
   getProjects(): Observable<Project[]> {
-    return this.http.get<Project[]>(`${projectUrl}all`);
+    return this.http.get<Project[]>(`${projectUrl}/all`);
   }
 
   getProjectById(id: number): Observable<Project> {
-    return this.http.get<Project>(`${projectUrl}project/${id}`);
+    return this.http.get<Project>(`${projectUrl}/project/${id}`);
   }
 }
