afa031f34d9f0281066bb1132cc62535d51ecdc1
bruno13palhano <bruno13palhano@gmail.com>
2024-01-20 19:50:06 -0300

    Implement a method to get the profile by id

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Profile } from '../model/profile';
+
+const profileUrl = 'http://localhost:8080/profile';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProfile(id: number): Observable<Profile> {
+    return this.http.get<Profile>(`${profileUrl}/profile/${id}`);
+  }
 }
