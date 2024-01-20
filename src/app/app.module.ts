74c15f44e6f5496756b81d55aa2c5a2b9ffe46e2
bruno13palhano <bruno13palhano@gmail.com>
2024-01-19 21:29:50 -0300

    Add ProfileComponent to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SortPipe } from './sort.pipe';
 import { CopyrightDirective } from './copyright.directive';
 import { ProjectsModule } from './projects/projects.module';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ProfileComponent } from './profile/profile/profile.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SortPipe,
     CopyrightDirective,
-    PortfolioComponent,
     ProfileComponent
   ],
   imports: [
